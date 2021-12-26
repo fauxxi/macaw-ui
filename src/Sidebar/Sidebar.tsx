2214006269c80a5c804c12d1f5da1a1faba63db2
Fauzi A Sanjaya <fauxxi.as@gmail.com>
2021-12-26 18:39:39 +0100

    Update Sidebar.tsx
    remove overflow, as it will create unnecessary scroll bar for smaller browser view

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(
       height: "100vh",
       position: "fixed",
       paddingRight: "2em",
-      overflowY: "auto",
+      //overflowY: "auto",
     },
     logo: {
       margin: `36px 0 ${theme.spacing(3)} ${theme.spacing(3.5)}`,
